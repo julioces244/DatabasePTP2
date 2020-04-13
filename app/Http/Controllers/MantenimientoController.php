63dc2383777ab9fe8ad483b4dcdb48db47c93cef
Julio César <julio.apaza@tecsup.edu.pe>
2020-04-13 00:55:39 -0500

    Commit Abril last1.4

--- app/Http/Controllers/MantenimientoController.php
+++ app/Http/Controllers/MantenimientoController.php
@@ -13,11 +13,11 @@ class MantenimientoController extends Controller
 
       try{
         $date = Carbon::now();
-        $date = $date->format('d-m-Y');
+        //$date = $date->format('d-m-Y');
 
         $mantenimientos = DB::table('mantenimientos')->where('colegios_idcolegio',$idcolegio)->get();
-        return $mantenimientos;
-        //return response()->json(['mantenimiento' => $mantenimientos, 'fecha' => $date]);
+        //return $mantenimientos;
+        return response()->json(['mantenimiento' => $mantenimientos, 'fecha' => $date]);
 
       }catch(\Exception $e){
 
--- database/seeds/MantenimientosTableSeeder.php
+++ database/seeds/MantenimientosTableSeeder.php
@@ -17,65 +17,86 @@ class MantenimientosTableSeeder extends Seeder
     {
       DB::table('mantenimientos')->insert([
           'idMantenimiento' => 1,
-          'descripcion' => 'Mantenimiento lab',
+          'descripcion' => 'Luis Fajardo - King',
           'flimite' => Carbon::parse('15-04-2020'),
           'colegios_idcolegio' => 1,
       ]);
 
       DB::table('mantenimientos')->insert([
           'idMantenimiento' => 2,
-          'descripcion' => 'Mantenimiento lab',
+          'descripcion' => 'Melissa - Fatima - Erika',
           'flimite' => Carbon::parse('17-04-2020'),
           'colegios_idcolegio' => 1,
       ]);
 
       DB::table('mantenimientos')->insert([
           'idMantenimiento' => 3,
-          'descripcion' => 'Mantenimiento lab',
+          'descripcion' => 'Gene - Yil',
           'flimite' => Carbon::parse('20-04-2020'),
           'colegios_idcolegio' => 1,
       ]);
 
       DB::table('mantenimientos')->insert([
           'idMantenimiento' => 4,
-          'descripcion' => 'Mantenimiento lab',
-          'flimite' => Carbon::parse('15-04-2020'),
+          'descripcion' => 'Luis Fajardo - King',
+          'flimite' => Carbon::parse('13-04-2020'),
           'colegios_idcolegio' => 2,
       ]);
 
       DB::table('mantenimientos')->insert([
           'idMantenimiento' => 5,
-          'descripcion' => 'Mantenimiento lab',
-          'flimite' => Carbon::parse('15-04-2020'),
+          'descripcion' => 'Melissa - Fatima - Erika',
+          'flimite' => Carbon::parse('14-04-2020'),
           'colegios_idcolegio' => 2,
       ]);
 
       DB::table('mantenimientos')->insert([
           'idMantenimiento' => 6,
-          'descripcion' => 'Mantenimiento lab',
+          'descripcion' => 'Gene - Yil',
           'flimite' => Carbon::parse('15-04-2020'),
           'colegios_idcolegio' => 2,
       ]);
 
       DB::table('mantenimientos')->insert([
           'idMantenimiento' => 7,
-          'descripcion' => 'Mantenimiento lab',
+          'descripcion' => 'Luis Fajardo - King',
           'flimite' => Carbon::parse('15-04-2020'),
           'colegios_idcolegio' => 3,
       ]);
 
       DB::table('mantenimientos')->insert([
           'idMantenimiento' => 8,
-          'descripcion' => 'Mantenimiento lab',
-          'flimite' => Carbon::parse('15-04-2020'),
+          'descripcion' => 'Melissa - Fatima - Erika',
+          'flimite' => Carbon::parse('16-04-2020'),
           'colegios_idcolegio' => 3,
       ]);
 
       DB::table('mantenimientos')->insert([
           'idMantenimiento' => 9,
-          'descripcion' => 'Mantenimiento lab',
-          'flimite' => Carbon::parse('15-04-2020'),
+          'descripcion' => 'Gene - Yil',
+          'flimite' => Carbon::parse('18-04-2020'),
           'colegios_idcolegio' => 3,
+      ]);
+
+      DB::table('mantenimientos')->insert([
+          'idMantenimiento' => 10,
+          'descripcion' => 'Luis Fajardo - King',
+          'flimite' => Carbon::parse('13-04-2020'),
+          'colegios_idcolegio' => 4,
+      ]);
+
+      DB::table('mantenimientos')->insert([
+          'idMantenimiento' => 11,
+          'descripcion' => 'Melissa - Fatima - Erika',
+          'flimite' => Carbon::parse('16-04-2020'),
+          'colegios_idcolegio' => 4,
+      ]);
+
+      DB::table('mantenimientos')->insert([
+          'idMantenimiento' => 12,
+          'descripcion' => 'Gene - Yil',
+          'flimite' => Carbon::parse('17-04-2020'),
+          'colegios_idcolegio' => 4,
       ]);
     }
 }
